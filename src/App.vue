90d13908395c4a6c1d0e01ff148674711ee026da
Viljar Kargenberg <viljark@gmail.com>
2016-05-28 16:11:28 +0300

    Vuejs & Webpack

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <main id="app">
+    <section>
+      <img class="logo" src="./assets/logo.png">
+      <hello></hello>
+      <p>
+        This is a sample app using <a target="_blank" href="http://vuejs-templates.github.io/webpack" target="_blank">vuejs webpack templates</a>.
+      </p>
+    </section>
+  </main>
+  <v-footer></v-footer>
+</template>
+
+<script>
+import Hello from './components/Hello'
+import VFooter from './components/VFooter'
+
+export default {
+  components: {
+    Hello,
+    VFooter
+  }
+}
+</script>
+
+<style lang="scss">
+html {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  height: 100%;
+  color: #2c3e50;
+  font-family: Roboto Slab, Source Sans Pro, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+main {
+  flex: 1;
+  max-width: 600px;
+  text-align: center;
+  margin: 10vh auto 1vh;
+}
+a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+p {
+  margin: 0 10px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
---
+++ src/components/VFooter.vue
@@ -0,0 +1,65 @@
+<template>
+  <footer class="footer">
+    <div class="footer__container">
+      <ul class="footer__list">
+        <li class="footer__item">
+          Viljar K.
+        </li>
+        <li class="footer__item">
+          FE Developer @ <strong><a href="http://iglu.ee">Iglu</a></strong>
+        </li>
+        <li class="footer__item">
+          <a href="https://www.linkedin.com/in/viljar-kärgenberg-81378398">LinkedIn</a>
+        </li>
+        <li class="footer__item">
+          Twitter: <strong><a href="https://twitter.com/ajateenija">@ajateenija</a></strong>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue.js & Webpack!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.footer {
+  border-top: 2px solid #42b983;
+  background: #fefefe;
+  padding: 20px;
+}
+.footer__list {
+  max-width: 650px;
+  margin: 0 auto;
+  padding: 0;
+  display: flex;
+  flex-direction: row;
+  list-style: none;
+}
+@media all and (max-width: 500px) {
+  .footer__list {
+    flex-direction: column;
+    font-size: 12px;
+  }
+  .footer__item {
+    padding-bottom: 3px;
+  }
+}
+.footer__item {
+  width: 100%;
+  text-align: center;
+}
+
+</style>
